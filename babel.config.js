8e37c1e8888f484aa0edcf3a1ff107bcd3b3c868
Sunny Golovine <sunny@glvn.co>
2020-01-20 17:41:43 -0500

    Updated babel config

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  presets: [
-    "@babel/preset-env",
-    "@babel/preset-react"
-  ], 
+  presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
-
-  ]
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src/'],
+      },
+    ],
+  ],
 }
