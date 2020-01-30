5012a58d834df67c15f0df35aa97657c35d8c8fa
Sunny Golovine <sunny@glvn.co>
2020-01-29 21:09:51 -0500

    Delete all components

--- src/content.js
+++
@@ -1,7 +0,0 @@
-export const header = "Sunny Golovine";
-
-export const subheader =
-  "Software Engineer specializing in Mobile Applications and React Native";
-
-export const bio =
-  "My name is Arseniy Avery (I go by Sunny Golovine). I am a Web and Mobile developer specializing in React Native. I have worked for several companies in the past including OnQ and Cox Automotive where I helped build exceptional mobile applications.";
