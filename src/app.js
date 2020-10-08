4e8ca3af04a42657f651091ec99a5459f5f25c79
Sunny Golovine <sunny@glvn.co>
2020-10-07 21:50:56 -0400

    Update about copy

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ import GithubIcon from "./svg/github";
 import TwitterIcon from "./svg/twitter";
 import LinkedInIcon from "./svg/linkedin";
 
+const SectionHeader = ({ title }) => (
+  <div className="py-4">
+    <h2 className="text-lg">{title}</h2>
+  </div>
+);
+
 const App = () => (
   <div className="container mx-auto flex flex-col items-left py-16 px-4 max-w-md">
     <div className="py-4 flex flex-col items-center">
@@ -14,26 +20,37 @@ const App = () => (
     </div>
     <h2 className="text-lg">About Me</h2>
     <p className="text-md max-w-md">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-      facilisis erat eu mollis pharetra. Integer ornare justo sed urna faucibus
-      commodo. Sed ac venenatis erat. In ultricies, mi ac ornare suscipit, ipsum
-      mauris sollicitudin lectus, in consequat felis ipsum nec erat. Aliquam
-      ultrices nisl diam, et rhoncus justo aliquam eu. Phasellus consectetur
-      gravida metus. Curabitur quis porta magna.
+      Hey! My name is Arseniy Golovine. I’m a Software Engineer currently living
+      in Atlanta, GA. I spend my weekends either Kayaking some of the lakes
+      around Atlanta or working on my side hustle Webhost Watchdog.
     </p>
-    <div className="py-4">
-      <h2 className="text-lg">Contact Me</h2>
-    </div>
+    <SectionHeader title="Contact Me" />
     <AddToContactsButton />
     <LinkButton
       title="Email"
       href="mailto:sunny@glvn.co"
       IconComponent={EmailIcon}
     />
-    <LinkButton title="Instagram" href="#" IconComponent={InstagramIcon} />
-    <LinkButton title="Github" href="#" IconComponent={GithubIcon} />
-    <LinkButton title="Twitter" href="#" IconComponent={TwitterIcon} />
-    <LinkButton title="Linkedin" href="#" IconComponent={LinkedInIcon} />
+    <LinkButton
+      title="Instagram"
+      href="https://instagram.com/sgolovine"
+      IconComponent={InstagramIcon}
+    />
+    <LinkButton
+      title="Github"
+      href="https://github.com/sgolovine"
+      IconComponent={GithubIcon}
+    />
+    <LinkButton
+      title="Twitter"
+      href="https://twitter.com/_glvn"
+      IconComponent={TwitterIcon}
+    />
+    <LinkButton
+      title="Linkedin"
+      href="https://linkedin.com/in/SunnyGolovine"
+      IconComponent={LinkedInIcon}
+    />
   </div>
 );
 
