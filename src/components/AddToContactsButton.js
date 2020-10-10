a1976639152bf26253536664983f94ce71aa4808
Sunny Golovine <sunny@glvn.co>
2020-10-09 20:05:47 -0400

    Fix contact not opening in instagram
    Fix background color on Android

--- src/components/AddToContactsButton.js
+++ src/components/AddToContactsButton.js
@@ -3,6 +3,8 @@ import AddContactIcon from "../svg/addContact";
 export const AddToContactsButton = () => (
   <a
     href="/assets/contact_card.vcf"
+    target="_blank"
+    rel="noreferrer noopener"
     className="flex flex-row items-start justify-start p-4 border max-w-sm text-center bg-blue-600 my-2"
   >
     <div className="w-6">
